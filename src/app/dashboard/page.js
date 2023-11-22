0c9ee1f4413f874bea0b07580ca40c6298925e5a
codeitfast <micahkurlandcohen@gmail.com>
2023-11-21 16:18:00 -0800

    animate, new icons

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -14,12 +14,12 @@ import { RiArrowDropDownLine } from "react-icons/ri";
 import { BsChevronDown } from "react-icons/bs";
 import { useState } from "react";
 import { Grid } from "@tremor/react";
+import { DashboardIcon, ListBulletIcon, ChevronDownIcon } from '@radix-ui/react-icons'
+import { motion } from 'framer-motion'
 
-
-
-const FakeCard = ({listView}) => {
+const FakeCard = ({ listView }) => {
   return (
-    <Card className={`grow ${listView == false? '' : ''} hover:shadow-md transition-all duration-250`}>
+    <Card className={`grow ${listView == false ? '' : ''} hover:shadow-md transition-all duration-250`}>
       <div className="flex">
         <a href="./dashboard/project-1" className="w-full">
           <div>
@@ -56,21 +56,34 @@ export default function Home() {
       <div className="w-[800px] mx-auto max-w-full p-2">
         <div className="flex">
           <TextInput className="" placeholder="Search..." />
-          
+
           <div className="shadow-md flex w-fit rounded-md ml-2">
-          <div className={`w-8 h-8 flex rounded-md place-items-center justify-between items-center m-auto ml-1 cursor-pointer ${listView == false? 'bg-slate-200' : ''}`}
-          onClick={()=>setListView(false)}
-          >
-          <svg data-testid="geist-icon" height="16" stroke-linejoin="round" viewBox="0 0 16 16" width="16" className={`transition-all mx-auto ${listView == true ? 'opacity-75 hover:opacity-100' : ''}`}><path fill-rule="evenodd" clip-rule="evenodd" d="M2.5 5.5V2.5H5.5V5.5H2.5ZM1 2C1 1.44772 1.44772 1 2 1H6C6.55228 1 7 1.44772 7 2V6C7 6.55228 6.55228 7 6 7H2C1.44772 7 1 6.55228 1 6V2ZM2.5 13.5V10.5H5.5V13.5H2.5ZM1 10C1 9.44772 1.44772 9 2 9H6C6.55228 9 7 9.44772 7 10V14C7 14.5523 6.55228 15 6 15H2C1.44772 15 1 14.5523 1 14V10ZM10.5 2.5V5.5H13.5V2.5H10.5ZM10 1C9.44772 1 9 1.44772 9 2V6C9 6.55228 9.44772 7 10 7H14C14.5523 7 15 6.55228 15 6V2C15 1.44772 14.5523 1 14 1H10ZM10.5 13.5V10.5H13.5V13.5H10.5ZM9 10C9 9.44772 9.44772 9 10 9H14C14.5523 9 15 9.44772 15 10V14C15 14.5523 14.5523 15 14 15H10C9.44772 15 9 14.5523 9 14V10Z" fill="currentColor" className="mx-auto"></path></svg>
-          </div>
-          <div className={`w-8 h-8 flex rounded-md place-items-center justify-between items-center m-auto mr-1 cursor-pointer ${listView == true? "bg-slate-200" : ''}`}
-          onClick={()=>setListView(true)}>
-          <FaListUl className={`transition-all mx-auto ${listView == false ? 'opacity-75 hover:opacity-100' : ''}`} />
-          </div>
+
+            <div className="relative w-10 h-10 cursor-pointer">
+              <motion.div className={`absolute w-8 h-8 flex rounded-md place-items-center justify-between items-center m-auto ml-1 cursor-pointer`}
+                onClick={() => setListView(false)}
+              >
+                <DashboardIcon className={`z-50 absolute transition-all mx-auto ml-2 mt-2 ${listView == true ? 'opacity-75 hover:opacity-100' : ''}`} />
+              </motion.div>
+              {listView != true ? (
+                <motion.div className="absolute bg-slate-200 w-8 h-8 m-auto top-1 left-1 rounded-md" layoutId="listView"></motion.div>
+              ) : null}
+            </div>
+
+            <motion.div className="relative w-10 h-10 cursor-pointer">
+              <motion.div className={`absolute w-8 h-8 flex rounded-md place-items-center justify-between items-center m-auto mr-1 cursor-pointer}`}
+                onClick={() => setListView(true)}>
+                <ListBulletIcon className={`z-50 transition-all mx-auto ml-3 mt-2 ${listView == false ? 'opacity-75 hover:opacity-100' : ''}`} />
+
+              </motion.div>
+              {listView == true ? (
+                <motion.div className="absolute bg-slate-200 w-8 h-8 m-auto top-1 left-1 rounded-md" layoutId="listView"></motion.div>
+              ) : null}
+            </motion.div>
           </div>
 
           <div className="dropdown ml-auto my-auto dropdown-end flex justify-center">
-            <Button className="!flex items-center place-content-center justify-center h-10 ml-2 rounded-full group"><span className="flex">Add New...<BsChevronDown className="h-full m-auto ml-4" /></span></Button>
+            <Button className="!flex items-center place-content-center justify-center h-10 ml-2 rounded-full group"><span className="flex">Add New...<ChevronDownIcon className="h-full m-auto ml-4" /></span></Button>
             <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52 translate-y-10">
               <li><a>Smaller thing</a></li>
               <li><a>Larger thing</a></li>
@@ -80,10 +93,10 @@ export default function Home() {
         </div>
 
 
-        <Grid className={`gap-2 mt-2 ${listView == false? "card-holder mt-4" : ""}`}>
-        <FakeCard listView={listView} />
-        <FakeCard listView={listView} />
-        <FakeCard listView={listView} />
+        <Grid className={`gap-2 mt-2 ${listView == false ? "card-holder mt-4" : ""}`}>
+          <FakeCard listView={listView} />
+          <FakeCard listView={listView} />
+          <FakeCard listView={listView} />
         </Grid>
 
 
